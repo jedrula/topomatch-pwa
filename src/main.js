3f59b90a179e2f873fb98bb4441ed00509d841a4
Andrzej Swaton <andrzej.swaton@gmail.com>
2025-05-16 23:45:36 +0200

    attempt to leave bare minimum

--- src/main.js
+++ src/main.js
@@ -1,27 +1,12 @@
 import "./assets/main.css";
 
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { pipeline } from "@huggingface/transformers";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
-
-async function runInference() {
-  try {
-    const pipe = await pipeline("sentiment-analysis");
-    const out = await pipe("I love transformers!");
-    console.log("Inference result:", out);
-  } catch (error) {
-    console.error("Inference error:", error);
-  }
-}
-
-runInference();
